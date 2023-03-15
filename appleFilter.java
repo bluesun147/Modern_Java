04de75e47ccb7293a74d6122dfe9e4b2af55145b
bluesun147 <bluesun147@naver.com>
2023-03-15 23:41:09 +0900

    second try : parameterize color! 메서드에 색 추가해서 색을 파라미터화 하자. 요구사항 변화에 더 유연하게 대응
    third try: 모든 속성으로 필터링
    -> 효과적인 방법은?

--- appleFilter.java
+++ appleFilter.java
@@ -17,10 +17,12 @@ public class appleFilter {
         inventory.add(apple3);
 
 
-        System.out.println(filterGreenApples(inventory).toString());
+        System.out.println(filterApplesByColor(inventory, Color.GREEN).toString());
     }
 
 
+    // 첫번째 시도
+    // 초록색 사과 필터링
     public static List<Apple> filterGreenApples(List<Apple> inventory) {
         List<Apple> result = new ArrayList<>(); // 사과 누적 리스트
         for (Apple apple : inventory) {
@@ -32,13 +34,47 @@ public class appleFilter {
         }
         return result;
     }
+
+    // 두번째 시도 (색을 파라미터화)
+    public static List<Apple> filterApplesByColor(List<Apple> inventory, Color color) {
+        List<Apple> result = new ArrayList<>(); // 사과 누적 리스트
+        for (Apple apple : inventory) {
+            System.out.println("!!");
+            if (apple.getColor().equals(color)) {
+                System.out.println("apple = " + apple);
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    // 무게로 필터링 하는 경우
+    // 색 필터링 코드와 대부분 중복! bad
+    public static List<Apple> filterApplesByWeight(List<Apple> inventory, int weight) {
+        List<Apple> result = new ArrayList<>(); // 사과 누적 리스트
+        for (Apple apple : inventory) {
+            System.out.println("!!");
+            if (apple.getWeight() > weight) {
+                System.out.println("apple = " + apple);
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    // 세번째 시도
+    // 색과 무게 필터 합치고 어떤 기준으로 필터링 할 지 flag 주는 방식? -> bad!
 }
 
 class Apple {
     public Color color;
+    public int weight;
 
     public Color getColor() {
         return color;
+    }
+    public int getWeight() {
+        return weight;
     }
 }
 
